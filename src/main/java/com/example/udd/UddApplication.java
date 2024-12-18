e605f0e5aea3846e3d5f3e487e8add1309a652b7
Nevena Gligorov <gligorov.nevena@gmail.com>
2024-12-18 21:22:38 +0100

    feat: dockerizing ELK + MinIO and MinIO setup in Java

--- src/main/java/com/example/udd/UddApplication.java
+++ src/main/java/com/example/udd/UddApplication.java
@@ -1,15 +1,37 @@
 package com.example.udd;
 
+import io.minio.MinioClient;
+import io.minio.errors.MinioException;
+import io.minio.messages.Bucket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import java.util.List;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class UddApplication {
 
 	public static void main(String[] args) {
+		MinioClient minioClient = demo();
+		try{
+			List<Bucket> buckets = minioClient.listBuckets();
+			System.out.println("Connection success, total buckets: " + buckets.size());
+		} catch (MinioException e){
+			System.out.println("Connection failed: " + e.getMessage());
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
 		SpringApplication.run(UddApplication.class, args);
+	}
+
+	private static MinioClient demo(){
+		MinioClient minioClient = MinioClient.builder()
+				.endpoint("http://localhost:9000")
+				.credentials("SVDdqWLflOqzWpVRqT2T", "lKzjUJ2VYYfUFx86YuP8rJLl6PcrnTWyLsxRF971")
+				.build();
+
+		return minioClient;
 	}
 
 }
